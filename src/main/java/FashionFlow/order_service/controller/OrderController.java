2cd4e2fdee903f85e74d69aae36497616612f951
Nimanthi Jayathilake <j.nimanthi@gtngroup.com>
2024-10-20 18:46:15 +0530

    Initiate the basic controllers, services and Repositories

---
+++ src/main/java/FashionFlow/order_service/controller/OrderController.java
@@ -0,0 +1,34 @@
+package FashionFlow.order_service.controller;
+
+
+import FashionFlow.order_service.dto.OrderRequest;
+import FashionFlow.order_service.service.OrderService;
+//import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+//import io.github.resilience4j.retry.annotation.Retry;
+//import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+@Slf4j
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return CompletableFuture.supplyAsync(() -> "Oops! Something went wrong, please order after some time!");
+    }
+}
---
+++ src/main/java/FashionFlow/order_service/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package FashionFlow.order_service.repository;
+
+import FashionFlow.order_service.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
